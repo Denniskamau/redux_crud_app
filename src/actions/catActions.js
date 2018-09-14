de829a14a6a8a63b525166c296ebf9aba865df07
Dennis <denniskamau3@gmail.com>
2018-09-14 11:25:15 +0300

    add webpack configurations

---
+++ src/actions/catActions.js
@@ -0,0 +1,15 @@
+import catApi from '../api/catApi';
+
+export function loadCats() {  
+  return function(dispatch) {
+    return catApi.getAllCats().then(cats => {
+      dispatch(loadCatsSuccess(cats));
+    }).catch(error => {
+      throw(error);
+    });
+  };
+}
+
+export function loadCatsSuccess(cats) {  
+    return {type: 'LOAD_CATS_SUCCESS', cats};
+  }
