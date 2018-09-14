396b3bc1f5579da29d85e1206ce5d3121386a36f
Dennis <denniskamau3@gmail.com>
2018-09-14 11:55:27 +0300

    Add api data

--- src/api/catApi.js
+++ src/api/catApi.js
@@ -1,4 +1,5 @@
-class CatApi {  
+
+class CatsApi {
   static getAllCats() {
     return fetch('http://localhost:5000/api/v1/cats').then(response => {
       return response.json();
@@ -7,4 +8,51 @@ class CatApi {
     });
   }
 
-export default CatApi;  
+  static updateCat(cat) {
+    const request = new Request(`http://localhost:5000/api/v1/cats/${cat.id}`, {
+      method: 'PUT',
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      }), 
+      body: JSON.stringify({cat: cat})
+    });
+
+
+    return fetch(request).then(response => {
+      return response.json();
+    }).catch(error => {
+      return error;
+    });
+  }
+
+  static createCat(cat) {
+    const request = new Request('http://localhost:5000/api/v1/cats/', {
+      method: 'POST',
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      }), 
+      body: JSON.stringify({cat: cat})
+    });
+
+
+    return fetch(request).then(response => {
+      return response.json();
+    }).catch(error => {
+      return error;
+    });
+  }
+
+  static deleteCat(cat) {
+    const request = new Request(`http://localhost:5000/api/v1/cats/${cat.id}`, {
+      method: 'DELETE'
+    });
+
+    return fetch(request).then(response => {
+      return response.json();
+    }).catch(error => {
+      return error;
+    });
+  }
+}
+
+export default CatsApi;
--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
-import React, {PropTypes} from 'react';  
+import React, {PropTypes} from 'react';
 import Header from './common/Header';
+import {connect} from 'react-redux';
 
-class App extends React.Component {  
+class App extends React.Component {
   render() {
     return (
       <div className="container-fluid">
@@ -12,8 +13,8 @@ class App extends React.Component {
   }
 }
 
-App.propTypes = {  
+App.propTypes = {
   children: PropTypes.object.isRequired
 };
 
-export default App;  
+export default App;
